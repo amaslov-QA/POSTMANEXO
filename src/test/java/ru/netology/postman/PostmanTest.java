3920ecfedace59e11a865b376a8f3da66e432259
aa_maslov <smart.maslov@gmail.com>
2021-12-12 09:51:23 +0300

    add test

---
+++ src/test/java/ru/netology/postman/PostmanTest.java
@@ -0,0 +1,25 @@
+package ru.netology.postman;
+
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+
+class PostmanTest {
+
+    @Test
+    void shouldPostmanGet() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+}
