313682e8dc8cead1dee73b044d3f9f6dc89e56cb
aa_maslov <smart.maslov@gmail.com>
2021-12-14 14:29:43 +0300

    add

--- src/test/java/ru/netology/postman/PostmanTest.java
+++ src/test/java/ru/netology/postman/PostmanTest.java
@@ -20,6 +20,6 @@ class PostmanTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"));
+                .body("data", equalTo("some da"));
     }
 }
